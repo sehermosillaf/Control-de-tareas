af8bf5ea6201b014cc618ca52e2380edad4a2b15
sebastianhf <sehermosillaf@gmail.com>
2022-10-01 22:26:15 -0300

    Reemplaza constructores, getter y setter de clases modelo

--- src/main/java/com/portafolio/control/modelo/Rol.java
+++ src/main/java/com/portafolio/control/modelo/Rol.java
@@ -1,12 +1,17 @@
 package com.portafolio.control.modelo;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Table(name = "rol")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Rol {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,31 +23,4 @@ public class Rol {
     @ManyToMany(mappedBy = "roles",fetch = FetchType.LAZY)
     @JsonBackReference
     private Set<Usuario> usuarios;
-
-    public Rol() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Set<Usuario> getUsuarios() {
-        return usuarios;
-    }
-
-    public void setUsuarios(Set<Usuario> usuarios) {
-        this.usuarios = usuarios;
-    }
 }
--- src/main/java/com/portafolio/control/modelo/Usuario.java
+++ src/main/java/com/portafolio/control/modelo/Usuario.java
@@ -1,15 +1,17 @@
 package com.portafolio.control.modelo;
 
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
+import lombok.*;
 import javax.persistence.*;
 import java.util.Set;
 
 
 @Entity
 @Table(name = "usuario")
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,62 +39,4 @@ public class Usuario {
     )
     @JsonManagedReference
     private Set<Rol> roles;
-    public Usuario() {
-    }
-
-    public Usuario(Long id, String nombre, String apellido, String email, String password) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Rol> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Rol> roles) {
-        this.roles = roles;
-    }
 }
