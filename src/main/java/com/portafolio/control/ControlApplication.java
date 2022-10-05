ef896a095b839c3d09fd8283cac53c5d1629685d
sebastianhf <sehermosillaf@gmail.com>
2022-10-05 12:54:38 -0300

    Agrega CORS

--- src/main/java/com/portafolio/control/ControlApplication.java
+++ src/main/java/com/portafolio/control/ControlApplication.java
@@ -2,6 +2,9 @@ package com.portafolio.control;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ControlApplication {
@@ -10,4 +13,13 @@ public class ControlApplication {
 		SpringApplication.run(ControlApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8100","http://localhost:8200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
